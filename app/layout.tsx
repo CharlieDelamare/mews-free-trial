627cf7ef87aa3fde19cecd173b81803b3ed7533b
CharlieDelamare <64169058+CharlieDelamare@users.noreply.github.com>
2026-01-31 01:06:41 +0000

    Create root layout

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Mews Free Trial',
+  description: 'Start your free trial with Mews',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
